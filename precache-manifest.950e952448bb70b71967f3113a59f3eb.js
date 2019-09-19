53b402888dc5ff9b925c9dd879e85231635412db
unknown <tiger_za_@windowslive.com>
2019-09-19 16:44:16 +0700

    Updates

---
+++ precache-manifest.950e952448bb70b71967f3113a59f3eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8353b73d507e4428dd7402dd00e7b676",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6cafa2116b3699e73cce",
+    "url": "/monsters-rolodex/static/css/main.b635b742.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "6cafa2116b3699e73cce",
+    "url": "/monsters-rolodex/static/js/main.b792613c.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
